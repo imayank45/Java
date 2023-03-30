de5444575deb012d640f24d2d67ab913540f6bb5
Mayank Kathane <93125481+imayank45@users.noreply.github.com>
2023-03-30 13:23:31 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    
+    public static void main(String[] args) {
+         // Variables
+
+         String fname = "Mayank";
+         int age = 22;
+         String lname = " Kathane";
+ 
+         System.out.println("My name is "+fname+lname+" and I am "+age+" years old.");
+    }
+}
